e2acc23e2061c93d81cf25f916653974871dc99a
wangcl <wcl_orange@163.com>
2015-06-18 02:14:00 +0000

    xxxxxxxxxxx

--- frappe/templates/pages/login.py
+++ frappe/templates/pages/login.py
@@ -162,7 +162,7 @@ def login_via_weixin(code,appid,path):
     else:
         login_oauth_user({'email': userId + "@rd.com", }, provider=provider)
         authSucc(token, userId)
-    frappe.local.response["location"] = "desk#List/" + path
+    frappe.local.response["location"] = path
 
 
 def authSucc(token, userid):
